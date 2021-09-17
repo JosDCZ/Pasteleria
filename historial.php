0a887754de83bd11b8fdc4e586c490f2ff2102a5
Josué Cortez <90920993+JosDCZ@users.noreply.github.com>
2021-09-17 17:33:27 -0600

    cambiar la fecha

--- historial.php
+++ historial.php
@@ -201,7 +201,7 @@
                             <div class="container">
                               <div class="row">
                                 <div class="col-sm-3 text-left">
-                                  02/09/2021
+                                  02/09/2022
                                 </div>
                                 <div class="col-sm-3 text-left">
                                   <strong>$100.00</strong>
