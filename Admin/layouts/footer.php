b770517fba1374ce2ced7cb20003ef26a50b71f4
Brayan Palacios <pp16014@ues.edu.sv>
2021-11-11 02:15:54 -0800

    Arreglo de toda la plantilla y seccionada

---
+++ Admin/layouts/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="float-start">
+            <p>2021 &copy; Ecake</p>
+        </div>
+        <div class="float-end">
+            <p>Creado con mucho <span class="text-danger"><i class="bi bi-heart"></i></span> para <a href="https://github.com/JosDCZ/Pasteleria">ti</a></p>
+        </div>
+    </div>
+</footer>
---
+++ Admin/layouts/menuLateral.php
@@ -0,0 +1,41 @@
+<div id="sidebar" class="active">
+            <div class="sidebar-wrapper active">
+                <div class="sidebar-header">
+                    <div class="d-flex justify-content-between">
+                        <div class="logo">
+                            <a href="index.html"><img style="height: 100px; width: 250px;" src="assets/images/logo/logo4.png" alt="Logo" srcset=""></a>
+                        </div>
+                        <div class="toggler">
+                            <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="sidebar-menu">
+                    <ul class="menu">
+                        <li class="sidebar-title">Menú</li>
+
+                        <li class="sidebar-item active">
+                            <a href="index.php" class='sidebar-link'>
+                                <i class="bi bi-grid-fill"></i>
+                                <span>Tablero</span>
+                            </a>
+                        </li>
+
+                        <li class="sidebar-item">
+                            <a href="empresa.php" class='sidebar-link'>
+                                <i class="bi bi-briefcase-fill"></i>
+                                <span>Empresa</span>
+                            </a>
+                        </li>
+
+                        <li class="sidebar-item  ">
+                            <a href="empleados.php" class='sidebar-link'>
+                                <i class="bi bi-people-fill"></i>
+                                <span>Empleados</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+            </div>
+        </div>
